8ffbfd618d2b6658f8a4ef682282e04c68f64e88
EnergyNick <nsikachin@gmail.com>
2023-06-04 15:19:25 +0300

    Class input refactoring

--- Roguelike/InputSystem/Input.cs
+++ Roguelike/InputSystem/Input.cs
@@ -4,124 +4,76 @@ using Roguelike.VectorUtility;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.InteropServices;
-using System.Text;
-using System.Threading.Tasks;
-using System.Runtime.InteropServices;
-using System.Windows;
 using Microsoft.Xna.Framework;
 
-namespace Roguelike
+namespace Roguelike;
+
+public static class Input
 {
-    public static class Input
+    private static readonly Dictionary<Keys, KeyState> States = new();
+
+    public static bool IsPressed(Keys key) => States.TryGetValue(key, out var state) && state.Pressed;
+
+    public static bool IsDown(Keys key) => States.TryGetValue(key, out var state) && state.IsDown;
+
+    public static bool IsUp(Keys key) => States.TryGetValue(key, out var state) && state.IsUp;
+
+    [DllImport("user32.dll")]
+    private static extern IntPtr GetForegroundWindow();
+
+
+    private static IntPtr GetActiveWindow() => GetForegroundWindow();
+
+    [DllImport("user32.dll")]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool GetWindowRect(IntPtr hWnd, out RectangleInternal rectangle);
+
+    public static Vector2Int GetWindowCoordinate() =>
+        GetWindowRect(GetActiveWindow(), out var r)
+            ? new Vector2Int(r.Left, r.Top)
+            : Vector2Int.Zero;
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct RectangleInternal
     {
+        public int Left;        // x position of upper-left corner
+        public int Top;         // y position of upper-left corner
+        public int Right;       // x position of lower-right corner
+        public int Bottom;      // y position of lower-right corner
+    }
 
-        private static Dictionary<Keys, KeyState> states = new();
+    [StructLayout(LayoutKind.Sequential)]
+    public struct PointInternal
+    {
+        public int X;
+        public int Y;
 
-        public static bool IsPressed(Keys key)
+        public static implicit operator Vector2Int(PointInternal pointInternal) => new(pointInternal.X, pointInternal.Y);
+    }
+
+    [DllImport("user32.dll")]
+    public static extern bool GetCursorPos(out PointInternal lpPointInternal);
+
+    public static Vector2Int GetCursorPosition()
+    {
+        GetCursorPos(out var lpPointInternal);
+
+        return lpPointInternal - GetWindowCoordinate() - new Vector2Int(8, 36);
+    }
+
+    public static void Update()
+    {
+        var pressedKeys = Keyboard.GetState().GetPressedKeys();
+
+        foreach (var item in States.Where(item => !pressedKeys.Contains(item.Key)))
+            item.Value.Pressed = false;
+
+        foreach (var key in pressedKeys)
         {
-            if (states.TryGetValue(key, out var state))
-            {
-                return state.Pressed;
-            }
-            return false;
-        }
-
-        public static bool IsDown(Keys key)
-        {
-            if (states.TryGetValue(key, out var state))
-            {
-                return state.IsDown;
-            }
-            return false;
-        }
-
-        public static bool IsUp(Keys key)
-        {
-            if (states.TryGetValue(key, out var state))
-            {
-                return state.IsUp;
-            }
-            return false;
-        }
-
-        [DllImport("user32.dll")]
-        static extern IntPtr GetForegroundWindow();
-
-
-        private static IntPtr GetActiveWindow()
-        {
-            IntPtr handle = IntPtr.Zero;
-            return GetForegroundWindow();
-        }
-
-        [DllImport("user32.dll")]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool GetWindowRect(IntPtr hWnd, out RECT lpRect);
-
-        public static Vector2Int GetWindowCoordinate()
-        {
-            if (GetWindowRect(GetActiveWindow(), out RECT r))
-            {
-                return new Vector2Int(r.Left, r.Top);
-            }
-            return Vector2Int.Zero;
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct RECT
-        {
-            public int Left;        // x position of upper-left corner  
-            public int Top;         // y position of upper-left corner  
-            public int Right;       // x position of lower-right corner  
-            public int Bottom;      // y position of lower-right corner  
-        }
-
-        [StructLayout(LayoutKind.Sequential)]
-        public struct POINT
-        {
-            public int X;
-            public int Y;
-
-            public static implicit operator Vector2Int(POINT point)
-            {
-                return new Vector2(point.X, point.Y);
-            }
-        }
-
-        [DllImport("user32.dll")]
-        public static extern bool GetCursorPos(out POINT lpPoint);
-
-        public static Vector2Int GetCursorPosition()
-        {
-            POINT lpPoint;
-            GetCursorPos(out lpPoint);
-
-            return lpPoint - GetWindowCoordinate() - new Vector2Int(8, 36);
-        }
-
-        public static void Update()
-        {
-            var pressedKeys = Keyboard.GetState().GetPressedKeys();
-
-            foreach (var item in states)
-            {
-                if (!pressedKeys.Contains(item.Key))
-                {
-                    item.Value.Pressed = false;
-                }
-            }
-
-            foreach (var key in pressedKeys)
-            {
-                if (states.ContainsKey(key))
-                {
-                    states[key].Pressed = true;
-                }
-                else
-                {
-                    states.Add(key, new KeyState());
-                }
-            }
+            if (States.TryGetValue(key, out var state))
+                state.Pressed = true;
+            else
+                States.Add(key, new KeyState());
         }
     }
 }
